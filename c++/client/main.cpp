3998f8ba92d882aeb458dd4f8f13c61f7a26d05f
Johannes Bauer <jbauer@implen.de>
2025-03-17 22:01:29 +0100

    Do logging to cerr and all dump data to cout

--- c++/client/main.cpp
+++ c++/client/main.cpp
@@ -11,22 +11,22 @@ using namespace flatbuffers;
 
 int main() {
     // Create an RPC client
-    std::cout << "Create RpcClient" << std::endl;
+    std::cerr << "Create RpcClient" << std::endl;
     RpcClient client("127.0.0.1", 7777);
 
     // Create a FlatBuffer request
-    std::cout << "Create flatbuffers builder" << std::endl;
+    std::cerr << "Create flatbuffers builder" << std::endl;
     FlatBufferBuilder fbb;
 
     // Create a HelloRequest with GREETING type and message
-    std::cout << "Create request" << std::endl;
+    std::cerr << "Create request" << std::endl;
     RequestType request_type = RequestType_GREETING;
     auto message = fbb.CreateString("Hello, world!");
     auto request = CreateHelloRequest(fbb, request_type, message);
     fbb.Finish(request);
 
     // Send request and get response
-    std::cout << "Make RPC call" << std::endl;
+    std::cerr << "Make RPC call" << std::endl;
     const std::size_t size = MAX_SIZE;
     unsigned char response[size] = {0};
     std:size_t offset = 0;
@@ -38,14 +38,14 @@ int main() {
     }
 
     // Verify the response
-    std::cout << "Verify response" << std::endl;
+    std::cerr << "Verify response" << std::endl;
     auto verifier = Verifier(response + offset, size - offset);
     if (VerifyHelloResponseBuffer(verifier)) {
         auto helloResponse = GetHelloResponse(response + offset);
         auto result = helloResponse->result();
-        std::cout << "Received response with " << result->size() << " strings." << std::endl;
+        std::cerr << "Received response with " << result->size() << " strings." << std::endl;
     } else {
-        std::cout << "No valid response received." << std::endl;
+        std::cerr << "No valid response received." << std::endl;
     }
 
     return 0;
--- c++/client/rpc.cpp
+++ c++/client/rpc.cpp
@@ -25,6 +25,10 @@ RpcClient::~RpcClient() {
     close(sockfd_);
 }
 
+void dumpData(const char *buffer, int length) {
+    for (int i = 0; i < length; i++) std::cout << buffer[i];
+}
+
 void RpcClient::sendRequest(unsigned char *request, std::size_t request_size, unsigned char *response, std::size_t response_size, std::size_t& offset) {
     // Create HTTP POST request
     std::string http_req = "POST /RPC HTTP/1.1\r\n";
@@ -34,18 +38,31 @@ void RpcClient::sendRequest(unsigned char *request, std::size_t request_size, un
     
     // Send request
     send(sockfd_, http_req.c_str(), http_req.length(), 0);
+    dumpData(http_req.c_str(), http_req.length());
     send(sockfd_, reinterpret_cast<const char*>(request), request_size, 0);
-    
+    dumpData(reinterpret_cast<const char*>(request), request_size);
+
     // Receive response
     int bytes_received = recv(sockfd_, response, response_size, 0);
     if (bytes_received < 0) {
         throw std::runtime_error("Failed to receive response");
     }
-    
+    std::cerr << "sendRequest() ";
+    std::cerr << "bytes_received=" << bytes_received << std::endl;
+    dumpData(reinterpret_cast<const char*>(response), bytes_received);
+
     // Parse HTTP response and extract body
-    std::string header(reinterpret_cast<const char*>(request), bytes_received);
+    std::string header(reinterpret_cast<const char*>(response), bytes_received);
+    std::cerr << "sendRequest() ";
+    std::cerr << "received header:" << std::endl;
+    std::cerr << header << std::endl;
     offset = header.find("\r\n\r\n") + 4;
     if (offset == std::string::npos) {
         throw std::runtime_error("Invalid HTTP response");
     }
+
+    std::cerr << "sendRequest() ";
+    std::cerr << "request_size=" << request_size << " ";
+    std::cerr << "response_size=" << response_size << " ";
+    std::cerr << "offset=" << offset << std::endl;
 }
